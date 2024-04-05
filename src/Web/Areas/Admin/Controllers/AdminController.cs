362634aef7999a325ad5a65d3a1f57d78f450b22
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-04-05 12:33:49 +0300

    NiceAdmin Template Added

---
+++ src/Web/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.eShopWeb.Infrastructure.Identity;
+
+namespace Microsoft.eShopWeb.Web.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize]
+[Route("[controller]/[action]")]
+public class AdminController : Controller
+{
+    public IActionResult Dashboard()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> OrderList()
+    {
+
+
+        return View();
+    }
+}
